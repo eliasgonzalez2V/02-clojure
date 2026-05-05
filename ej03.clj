e12e4077f281f12e5382b1d4de88d5666b6b4efe
eliasgonzalez2V <elias.gonzalez@alu.inspt.utn.edu.ar>
2026-05-05 10:27:20 -0300

    ej03 corregido

--- ej03.clj
+++ ej03.clj
@@ -1,3 +1,3 @@
 (defn entero? [x]  (and (number? x)       (== x (Math/floor x))))
 (defn div10[x](/ x 10))
-(defn sig-mul-10 [a] (if (= (entero? (div10 a)) true) a (sig-mul-10 (+ a 1)) ) )
+(defn sig-mul-10 [a] (if (= a 0) 1 (if (= (entero? (div10 a)) true) a (sig-mul-10 (+ a 1)) ) ) )
